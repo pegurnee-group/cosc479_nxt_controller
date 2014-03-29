111e9ad312f76fc9f2df931b95efca2aa787bf92
Mike <mikenitchie@gmail.com>
2014-03-29 17:17:02 -0400

    Set up the tab view navigation.

--- FirstNXTProject/src/nitchie/arruda/gurnee/chiluka/firstnxtproject/MainActivity.java
+++ FirstNXTProject/src/nitchie/arruda/gurnee/chiluka/firstnxtproject/MainActivity.java
@@ -1,16 +1,39 @@
 package nitchie.arruda.gurnee.chiluka.firstnxtproject;
 
-import nitchie.arruda.gurnee.chiluka.firstnxtproject.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Resources;
 import android.view.Menu;
+import android.widget.TabHost;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.connect_view);
+		setContentView(R.layout.main_view);
+		
+		// Set up tabbars
+		Resources res = getResources();
+		final TabHost tabHost = (TabHost) findViewById(R.id.ui_1_TabHost);
+		
+	    // need setup since uses @+id/ui_1_TabHost instead of android:id/tabhost
+ 	    tabHost.setup();
+ 	    
+ 	    // Set up drive view tab
+ 	    TabHost.TabSpec spec = tabHost.newTabSpec("tag1");
+ 	    getLayoutInflater().inflate(R.layout.drive_view, tabHost.getTabContentView(), true);
+ 	    spec.setContent(R.id.drive_view_layout);
+ 	    spec.setIndicator("View");
+ 	    tabHost.addTab(spec);
+ 	    
+ 	    // Set up connect view tab
+ 	    spec = tabHost.newTabSpec("tag2");
+ 	    getLayoutInflater().inflate(R.layout.connect_view, tabHost.getTabContentView(), true);
+ 	    spec.setContent(R.id.connect_view_layout);
+ 	    spec.setIndicator("Connect");
+ 	    tabHost.addTab(spec);
+ 	    
 	}
 
 	@Override
