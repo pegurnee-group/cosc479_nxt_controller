15050061180b18fda963498015302db8650e18f6
Mike <mikenitchie@gmail.com>
2014-03-29 16:52:21 -0400

    Deleted garbage variables and updated the target SDK version to 19 so eclipse would stop showing me yellow triangles.

--- FirstNXTProject/src/nitchie/arruda/gurnee/chiluka/firstnxtproject/MainActivity.java
+++ FirstNXTProject/src/nitchie/arruda/gurnee/chiluka/firstnxtproject/MainActivity.java
@@ -11,8 +11,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		int b = 2;
-		int a = 9;
 	}
 
 	@Override
