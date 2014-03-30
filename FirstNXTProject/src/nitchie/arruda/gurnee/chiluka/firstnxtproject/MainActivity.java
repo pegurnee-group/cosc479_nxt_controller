ec4fd23b17089785bb130b5f357594ac351564ac
Mike <mikenitchie@gmail.com>
2014-03-29 21:30:59 -0400

    Connect and Disconnect buttons now toggle.

--- FirstNXTProject/src/nitchie/arruda/gurnee/chiluka/firstnxtproject/MainActivity.java
+++ FirstNXTProject/src/nitchie/arruda/gurnee/chiluka/firstnxtproject/MainActivity.java
@@ -12,6 +12,8 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,111 +24,134 @@ import android.widget.Button;
 import android.widget.TabHost;
 
 public class MainActivity extends Activity implements OnClickListener {
-	
+
 	private final String TAG = "NXT Project 1";
 	private final String ROBOTNAME = "herb-E";
-	
-	//Bluetooth Variables
+
+	// UI Components
+	Button connectButton;
+	Button disconnectButton;
+
+	// Bluetooth Variables
 	private BluetoothAdapter btInterface;
 	private Set<BluetoothDevice> pairedDevices;
 	private BluetoothSocket socket;
 	private BluetoothDevice bd;
 	private InputStream is = null;
 	private OutputStream os = null;
-	private BroadcastReceiver btMonitor = null;
+
+	// flag representing BT connection status
+	private boolean btConnected;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_view);
-		
+
+		btConnected = false;
+
 		setupTabs();
-		
-		Button connectButton = (Button) this.findViewById(R.id.connectButton);
+
+		connectButton = (Button) this.findViewById(R.id.connectButton);
 		connectButton.setOnClickListener(this);
-		
-		Button disconnectButton = (Button) this.findViewById(R.id.disconnectButton);
+
+		disconnectButton = (Button) this.findViewById(R.id.disconnectButton);
 		disconnectButton.setOnClickListener(this);
+		disconnectButton.setVisibility(View.GONE);
 
 	}
-	
+
 	public void setupTabs() {
 		// Set up tabbars
 		Resources res = getResources();
 		final TabHost tabHost = (TabHost) findViewById(R.id.ui_1_TabHost);
-		
-	    // need setup since uses @+id/ui_1_TabHost instead of android:id/tabhost
- 	    tabHost.setup();
- 	    
- 	    // Set up connect view tab
- 	    TabHost.TabSpec spec = tabHost.newTabSpec("tag1");
- 	    getLayoutInflater().inflate(R.layout.connect_view, tabHost.getTabContentView(), true);
- 	    spec.setContent(R.id.connect_view_layout);
- 	    spec.setIndicator("Connect");
- 	    tabHost.addTab(spec);
- 	    
- 	    // Set up drive view tab
- 	    spec = tabHost.newTabSpec("tag2");
- 	    getLayoutInflater().inflate(R.layout.drive_view, tabHost.getTabContentView(), true);
- 	    spec.setContent(R.id.drive_view_layout);
- 	    spec.setIndicator("Drive");
- 	    tabHost.addTab(spec);
+
+		// need setup since uses @+id/ui_1_TabHost instead of android:id/tabhost
+		tabHost.setup();
+
+		// Set up connect view tab
+		TabHost.TabSpec spec = tabHost.newTabSpec("tag1");
+		getLayoutInflater().inflate(R.layout.connect_view,
+				tabHost.getTabContentView(), true);
+		spec.setContent(R.id.connect_view_layout);
+		spec.setIndicator("Connect");
+		tabHost.addTab(spec);
+
+		// Set up drive view tab
+		spec = tabHost.newTabSpec("tag2");
+		getLayoutInflater().inflate(R.layout.drive_view,
+				tabHost.getTabContentView(), true);
+		spec.setContent(R.id.drive_view_layout);
+		spec.setIndicator("Drive");
+		tabHost.addTab(spec);
 	}
-	
+
 	public void onClick(View v) {
-		switch(v.getId()) {
-		case(R.id.connectButton):
+		switch (v.getId()) {
+		case (R.id.connectButton):
 			connectToDevice();
 			break;
-		case(R.id.disconnectButton):
-			
+		case (R.id.disconnectButton):
+			disconnectNXT(v);
 			break;
 		}
 	}
-	
+
 	public void connectToDevice() {
 		btInterface = BluetoothAdapter.getDefaultAdapter();
- 	    pairedDevices = btInterface.getBondedDevices();
- 	    Iterator<BluetoothDevice> it = pairedDevices.iterator();
- 	    while(it.hasNext()) {
- 	    	bd = it.next();
- 	    	
- 	    	if (bd.getName().equalsIgnoreCase(ROBOTNAME)) {
- 	 	    	try {
- 	 	    		socket = bd.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
- 	 	    		socket.connect();
- 	 	    	} catch (IOException e) {
- 	 	    		Log.e(TAG, "Error interacting with remote device -> " + e.getMessage());
- 	 	    		return;
- 	 	    	}
- 	 	    	
- 	 	    	try {
- 	 	    		is = socket.getInputStream();
- 	 	    		os = socket.getOutputStream();
- 	 	    	} catch (IOException e) {
- 	 	    		is = null;
- 	 	    		os = null;
- 	 	    		disconnectNXT(null);
- 	 	    		return;
- 	 	    	}
- 	 	    	
- 	 	    	Log.i(TAG, "Connected with " + bd.getName());
- 	 	    	return;
- 	    	}
- 	    }
+		pairedDevices = btInterface.getBondedDevices();
+		Iterator<BluetoothDevice> it = pairedDevices.iterator();
+		while (it.hasNext()) {
+			bd = it.next();
+
+			if (bd.getName().equalsIgnoreCase(ROBOTNAME)) {
+				try {
+					socket = bd
+							.createRfcommSocketToServiceRecord(UUID
+									.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+					socket.connect();
+				} catch (IOException e) {
+					Log.e(TAG,
+							"Error interacting with remote device -> "
+									+ e.getMessage());
+					return;
+				}
+
+				try {
+					is = socket.getInputStream();
+					os = socket.getOutputStream();
+				} catch (IOException e) {
+					is = null;
+					os = null;
+					disconnectNXT(null);
+					return;
+				}
+				
+ 	 	    	btConnected = true;
+ 	 	    	connectButton.setVisibility(View.GONE);
+ 	 	    	disconnectButton.setVisibility(View.VISIBLE);
+
+				Log.i(TAG, "Connected with " + bd.getName());
+				return;
+			}
+		}
 	}
-	
+
 	public void disconnectNXT(View v) {
 		try {
-			Log.i(TAG,"Attempting to break BT connection of " + bd.getName());
+			Log.i(TAG, "Attempting to break BT connection of " + bd.getName());
 			socket.close();
 			is.close();
 			os.close();
 			Log.i(TAG, "BT connection of " + bd.getName() + " is disconnected");
+		} catch (Exception e) {
+			Log.e(TAG, "Error in disconnect -> " + e.getMessage());
 		}
-		catch (Exception e)	{
-			Log.e(TAG,"Error in disconnect -> " + e.getMessage());
-		}
+
+		btConnected = false;
+		connectButton.setVisibility(View.VISIBLE);
+		disconnectButton.setVisibility(View.GONE);
+
 	}
 
 	@Override
