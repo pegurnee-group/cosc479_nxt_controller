99839e351e091275f000e56e6649eded0f3c473a
Eddie Gurnee <egurnee@emich.edu>
2014-04-01 18:21:48 -0400

    add onItemClickListener to the listView

--- FirstNXTProject/src/nitchie/arruda/gurnee/chiluka/firstnxtproject/PopupActivity.java
+++ FirstNXTProject/src/nitchie/arruda/gurnee/chiluka/firstnxtproject/PopupActivity.java
@@ -3,42 +3,52 @@ package nitchie.arruda.gurnee.chiluka.firstnxtproject;
 import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class PopupActivity extends ListActivity implements OnClickListener {
+public class PopupActivity extends ListActivity {
+	BluetoothDevice[] devices;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.popup_view);
-		
-		//String[] names = {"eddie", "was", "here", "don'tchu", "worry", "child" };
 
-		BluetoothDevice[] devices = (BluetoothDevice[]) BluetoothAdapter
-				.getDefaultAdapter().getBondedDevices().toArray();
-		
+		// String[] names = {"eddie", "was", "here", "don'tchu", "worry",
+		// "child" };
+
+		devices = (BluetoothDevice[]) BluetoothAdapter.getDefaultAdapter()
+				.getBondedDevices().toArray();
+
 		int limit = devices.length;
 		String[] names = new String[limit];
-		
+
 		for (int i = 0; i < limit; i++) {
 			names[i] = devices[i].getName() + "\n" + devices[i].getAddress();
 		}
-		
+
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				R.layout.row_view, R.id.row_lbl, names);
 		this.setListAdapter(adapter);
-		
-		Log.e("hello", "popup viewed");
+
+		((ListView) this.findViewById(R.id.popup_list))
+				.setOnItemClickListener(new ListItemListener());
+
 	}
-	
-	@Override
-	public void onClick(View v) {
-		Intent intent = new Intent(PopupActivity.this, MainActivity.class);
-	    this.startActivity(intent);
+
+	private class ListItemListener implements OnItemClickListener {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			DeviceData myObject = (DeviceData) DeviceData.getInstance();
+			myObject.setTheUUID(devices[position].getUuids()[0].getUuid());
+			PopupActivity.this.finish();
+		}
+
 	}
 }
