b2635846cd041fca4b99ce7ba6c3f1075142d522
Mike <mikenitchie@gmail.com>
2014-04-08 00:24:08 -0400

    Works! Just need to hide the menu bar.

--- Swipe4Seasons/src/com/example/swipe4seasons/MainActivity.java
+++ Swipe4Seasons/src/com/example/swipe4seasons/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends FragmentActivity implements
 		ActionBar.TabListener {
@@ -24,6 +25,28 @@ public class MainActivity extends FragmentActivity implements
 		
 		// Initilization
         myViewPager = (ViewPager) findViewById(R.id.pager);
+        
+        /**
+         * on swiping the viewpager make respective tab selected
+         * */
+        myViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+         
+            @Override
+            public void onPageSelected(int position) {
+                // on changing the page
+                // make respected tab selected
+                myActionBar.setSelectedNavigationItem(position);
+            }
+         
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+            }
+         
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+            }
+        });
+        
         myActionBar = getActionBar();
         myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
  
@@ -53,7 +76,7 @@ public class MainActivity extends FragmentActivity implements
 
 	@Override
 	public void onTabSelected(Tab tab, FragmentTransaction ft) {
-		// TODO Auto-generated method stub
+		myViewPager.setCurrentItem(tab.getPosition());
 
 	}
 
@@ -61,6 +84,21 @@ public class MainActivity extends FragmentActivity implements
 	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+          super.onWindowFocusChanged(hasFocus);
+      if (hasFocus) {
+    	  getWindow().getDecorView().setSystemUiVisibility(
+                  View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                  | View.SYSTEM_UI_FLAG_FULLSCREEN
+                  | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+           );
+      }
 	}
 
 }
