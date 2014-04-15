3d1d7b777d0b25214ef65208ea985f09afcce8b0
Nicole Arruda <narruda@emich.edu>
2014-04-14 23:05:39 -0400

    sensor_image_selector.xml to control sensor images

--- FirstNXTProject/src/nitchie/arruda/gurnee/chiluka/firstnxtproject/SensorActivity.java
+++ FirstNXTProject/src/nitchie/arruda/gurnee/chiluka/firstnxtproject/SensorActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -30,6 +31,12 @@ public class SensorActivity extends Fragment {
             Bundle savedInstanceState) {
 		
 		rootView = inflater.inflate(R.layout.sensor_view, container, false);
+		
+		/*
+		 * Not sure if this is how to go about this...?  No setLevel() for ImageView?
+		 */
+		ImageView sensorImg = (ImageView) rootView.findViewById(R.id.sensor_image);
+//		sensorImg.setLevel(0);
 		
 		return rootView;
 	}
