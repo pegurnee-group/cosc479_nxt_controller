c2ccf860be18d5b096b4f03e8eb0cda08281b7bd
Eddie Gurnee <egurnee@emich.edu>
2014-04-14 20:28:58 -0400

    let's attempt gyroscope

---
+++ FirstNXTProject/src/nitchie/arruda/gurnee/chiluka/firstnxtproject/GyroscopeActivity.java
@@ -0,0 +1,22 @@
+package nitchie.arruda.gurnee.chiluka.firstnxtproject;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.support.v4.app.Fragment;
+
+public class GyroscopeActivity extends Fragment implements SensorEventListener {
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
